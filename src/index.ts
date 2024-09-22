987a9f59c2d3ed19990acf6cb2316b87b6367ef5
Decodam <arghyamondal.work@gmail.com>
2024-09-22 20:49:43 +0530

    cors added

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,19 @@ import { authHandler, initAuthConfig, verifyAuth } from '@hono/auth-js'
 import { Hono } from 'hono'
 import getAuthConfig from './services/auth'
 import ProtectedRoutes from './routes/protected.routes'
+import { cors } from 'hono/cors'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings: {CORS_ORIGIN:string}
+}>()
+
+// cors config
+app.use('*', async (c, next) => {
+  const corsMiddlewareHandler = cors({
+    origin: c.env.CORS_ORIGIN,
+  })
+  return corsMiddlewareHandler(c, next)
+})
 
 // auth middleware
 app.use("*", initAuthConfig(getAuthConfig))
